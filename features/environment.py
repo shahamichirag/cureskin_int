79332fe178386dc14a4880f982038f349851a0a9
Ami Shah <shahamichirag@gmail.com>
2023-05-03 10:25:45 -0400

    BrowserStack cloud testing on Safari and  Firefox

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from app.application import Application
+from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.chrome.options import Options
 
-def browser_init(context):
+
+def browser_init(context, test_name):
     """
     :param context: Behave context
     """
@@ -22,17 +24,44 @@ def browser_init(context):
 
     #--------------HEADLESS MODE CODE AS BELOW--------------
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--start-maximized')
-    options.add_argument('--headless')
-    context.driver = webdriver.Chrome(chrome_options=options,executable_path='./chromedriver.exe')
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--window-size=1920,1080')
+    # options.add_argument('--start-maximized')
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(chrome_options=options,executable_path='./chromedriver.exe')
+    # context.app = Application(context.driver)
+
+    #------------------BROWSER STACK FOR CROSS BROWSER TESTING----------------------
+
+    bs_user = ''
+    bs_key = ''
+
+    # desired_cap = {
+    #     "browserName": "Firefox",
+    #     "browserVersion": "102.0",
+    #     "os": "Windows",
+    #     "osVersion": "10",
+    #     'name': test_name
+    # }
+    desired_cap = {
+        "browserName": "Safari",
+        "browserVersion": "16.3",
+        "os": "MAC",
+        "osVersion": "Ventura",
+        'name': test_name
+    }
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(5)
+    context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
